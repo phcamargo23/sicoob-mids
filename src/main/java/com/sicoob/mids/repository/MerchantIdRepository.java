258c8ed98652b96417ac1270778e7637b9b09226
PedroG.Camargo <pedrog.camargo@fornecedores.sicoob.com.br>
2026-09-22 21:14:18 -0300

    Ajusta nome dos objetos para se adequar à política do Sicoob.

--- src/main/java/com/sicoob/mids/repository/MerchantIdRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sicoob.mids.repository;
-
-import com.sicoob.mids.model.MerchantId;
-import com.sicoob.mids.model.MerchantIdPK;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MerchantIdRepository extends JpaRepository<MerchantId, MerchantIdPK> {
-}
